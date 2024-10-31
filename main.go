397439ce9dfac5f32ad61d5d7c1fcc6ff8631d85
imamimaniman <namasayaiman02@gmail.com>
2024-10-31 16:08:20 +0700

    const

--- main.go
+++ main.go
@@ -1,68 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	// var firstName string = "John";
+	const (
+		square = "kotak"
+		isTodayFriday = true
+		numeric uint8 = 255
+		floatNum = 2.2
+	)
 
-	// var lastName string;
-	// lastName = "Doe";
+	const (
+		a = "test"
+		b
+	)
 
-	// thirdName := "Smith";
+	const (
+    today string = "senin"
+    sekarang
+    isToday2 = true
+	)
 
-	// var first, second, third string;
-	// first, second, third = "John", "Doe", "Smith";
+	fmt.Println(square)
+	fmt.Println(isTodayFriday)
+	fmt.Println(numeric)
+	fmt.Println(a)
+	fmt.Println(b)
 
-	// var fourth, fifth, sixth string = "John1", "Doe1", "Smith1";
+	const satu, dua = 1, 2
+	const three, four string = "tiga", "empat"
 
-	// seventh, eighth, ninth := "John2", "Doe2", "Smith2";
-
-	// one, isFriday, twoPointTwo, say := 1, true, 2.2, "hello";
-
-	// name := new(string)
-	// boolEan := new(bool)
-	// intEger := new(int)
-
-	// fmt.Println("name string", name);
-	// fmt.Println("boolEan bool", boolEan);
-	// fmt.Println("intEger int", intEger);
-
-	// fmt.Println(firstName, lastName, thirdName);
-	// fmt.Println(first, second, third);
-	// fmt.Println(fourth, fifth, sixth);
-	// fmt.Println(seventh, eighth, ninth);
-	// fmt.Println(one, isFriday, twoPointTwo, say);
-
-	// fmt.Printf("halo %s %s %s!\n", firstName, lastName, thirdName)
-	// fmt.Println("halo", firstName, lastName, thirdName + "! \n")
-
-	// thirdName = "Wick";
-
-	// fmt.Printf("halo %s %s %s!\n", firstName, lastName, thirdName)
-	// fmt.Println("halo", firstName, lastName, thirdName + "! \n")
-
-	// var positiveNumber uint8 = 89
-	// var negativeNumber = -1243423644
-
-	// fmt.Printf("bilangan positif: %d\n", positiveNumber)
-	// fmt.Printf("bilangan negatif: %d\n", negativeNumber)
-
-	// var decimalNumber = 2.62;
-	// var decimalNumber2 float32 = 3.14;
-	// var decimalNumber3 float64 = 8.999999999999999;
-	// decimalNumber4 := 5.0;
-
-
-	// fmt.Printf("bilangan desimal: %f\n", decimalNumber)
-	// fmt.Printf("bilangan desimal: %f\n", decimalNumber2)
-	// fmt.Printf("bilangan desimal: %f\n", decimalNumber3)
-	// fmt.Printf("bilangan desimal: %f\n", decimalNumber4)
-	// fmt.Println("bilangan desimal: ", decimalNumber);
-
-	exist := true;
-	exist2 := false;
-
-	fmt.Println("exist?", exist);
-	fmt.Printf("exist2? %t\n", exist2);
-
+	fmt.Println(satu)
+	fmt.Println(dua)
 }
