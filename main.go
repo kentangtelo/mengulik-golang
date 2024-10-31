0fac96ce24296e8799fcc5816bb50973bccb2393
imamimaniman <namasayaiman02@gmail.com>
2024-10-31 22:14:24 +0700

    for loop star

--- main.go
+++ main.go
@@ -5,35 +5,189 @@ import (
 )
 
 func main() {
-	var xs ="abcdefghijklmnopqrstuvwxyz"
+	for i := 0; i < 10; i++ {
+		if i%2==1 {
+			continue
+		}
 
-	for i, v := range xs {
-		fmt.Println(i, string(v))
+		if i>8 {
+			break
+		}
+
+		fmt.Println("angka", i)
 	}
 
-	var ys = [5]int{1, 2, 3, 4, 5}
 
-	for _, v := range ys {
-		fmt.Println(v)
+	fmt.Println()
+	/*
+	*****
+	****
+	***
+	**
+	*
+	*/
+
+	for i := 0; i < 5; i++ {
+		for j := i; j < 5; j++ {
+			fmt.Print("*")
+		}
+
+		fmt.Println()
 	}
 
-	var zs = ys[1:3]
+	fmt.Println()
+	/*
+	*
+	**
+	***
+	****
+	*****
+	*/
 
-	for _, v := range zs {
-		fmt.Println(v)
+	for i := 0; i < 5; i++ {
+		for j := 0; j <= i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
 	}
 
-	var kvs = map[byte]int{'a': 1, 'b': 2, 'c': 3}
-	for k, v := range kvs {
-		fmt.Println(string(k), v)
+	fmt.Println()
+
+	/*
+	*
+	**
+	***
+	****
+	*****
+	****
+	***
+	**
+	*
+	*/
+
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 5; j++ {
+			if i < 5 {
+				if j <= i {
+					fmt.Print("*")
+				}
+			} else {
+				if j < 8-i {
+					fmt.Print("*")
+				}
+			}
+		}
+		fmt.Println()
 	}
 
-	for range kvs {
-		fmt.Println("Done")
+	fmt.Println()
+
+	/*
+	*****
+	 ****
+	  ***
+		 **
+		  *
+	*/
+
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			if j >= i {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
 	}
 
-	for i:= range 5{
-		fmt.Println(i)
+	fmt.Println()
+
+	/*
+	    *
+	   **
+	  ***
+	 ****
+	*****
+	*/
+
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			if j >= 4-i {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
 	}
+
+	fmt.Println()
+
+	/*
+	    *
+	   **
+	  ***
+	 ****
+	*****
+	 ****
+	  ***
+		 **
+		  *
+	*/
+
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 5; j++ {
+			if i < 5 {
+				if j >= 4-i {
+					fmt.Print("*")
+				} else {
+					fmt.Print(" ")
+				}
+			} else {
+				if j >= i-4 {
+					fmt.Print("*")
+				} else {
+					fmt.Print(" ")
+				}
+			}
+		}
+		fmt.Println()
+	}
+
+	/*
+	    *
+	    **
+	   ****
+	  ******
+	 ********
+	**********
+	 ********
+	  ******
+		 ****
+		  **
+			*
+	*/
+
+	for i := 0; i < 11; i++ {
+		for j := 0; j < 11; j++ {
+			if i < 6 {
+				if j >= 5-i && j <= 5+i {
+					fmt.Print("*")
+				} else {
+					fmt.Print(" ")
+				}
+			} else {
+				if j >= i-5 && j <= 15-i {
+					fmt.Print("*")
+				} else {
+					fmt.Print(" ")
+				}
+			}
+		}
+		fmt.Println()
+	}
+
+	fmt.Println()
 
 }
