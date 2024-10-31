d548152365db37cadb2f2fb36a1005350061c01d
imamimaniman <namasayaiman02@gmail.com>
2024-10-31 15:17:47 +0700

    tipe data numerik

--- main.go
+++ main.go
@@ -2,44 +2,60 @@ package main
 
 import "fmt"
 
+func main() {
+	// var firstName string = "John";
 
-func main()  {
-	var firstName string = "John";
+	// var lastName string;
+	// lastName = "Doe";
 
-	var lastName string;
-	lastName = "Doe";
+	// thirdName := "Smith";
 
-	thirdName := "Smith";
+	// var first, second, third string;
+	// first, second, third = "John", "Doe", "Smith";
 
-	var first, second, third string;
-	first, second, third = "John", "Doe", "Smith";
+	// var fourth, fifth, sixth string = "John1", "Doe1", "Smith1";
 
-	var fourth, fifth, sixth string = "John1", "Doe1", "Smith1";
+	// seventh, eighth, ninth := "John2", "Doe2", "Smith2";
 
-	seventh, eighth, ninth := "John2", "Doe2", "Smith2";
+	// one, isFriday, twoPointTwo, say := 1, true, 2.2, "hello";
 
-	one, isFriday, twoPointTwo, say := 1, true, 2.2, "hello";
+	// name := new(string)
+	// boolEan := new(bool)
+	// intEger := new(int)
 
-	name := new(string)
-	boolEan := new(bool)
-	intEger := new(int)
+	// fmt.Println("name string", name);
+	// fmt.Println("boolEan bool", boolEan);
+	// fmt.Println("intEger int", intEger);
 
-	fmt.Println("name string", name);
-	fmt.Println("boolEan bool", boolEan);
-	fmt.Println("intEger int", intEger);
+	// fmt.Println(firstName, lastName, thirdName);
+	// fmt.Println(first, second, third);
+	// fmt.Println(fourth, fifth, sixth);
+	// fmt.Println(seventh, eighth, ninth);
+	// fmt.Println(one, isFriday, twoPointTwo, say);
 
-	fmt.Println(firstName, lastName, thirdName);
-	fmt.Println(first, second, third);
-	fmt.Println(fourth, fifth, sixth);
-	fmt.Println(seventh, eighth, ninth);
-	fmt.Println(one, isFriday, twoPointTwo, say);
+	// fmt.Printf("halo %s %s %s!\n", firstName, lastName, thirdName)
+	// fmt.Println("halo", firstName, lastName, thirdName + "! \n")
 
-	fmt.Printf("halo %s %s %s!\n", firstName, lastName, thirdName)
-	fmt.Println("halo", firstName, lastName, thirdName + "! \n")
+	// thirdName = "Wick";
 
-	thirdName = "Wick";
+	// fmt.Printf("halo %s %s %s!\n", firstName, lastName, thirdName)
+	// fmt.Println("halo", firstName, lastName, thirdName + "! \n")
 
-	fmt.Printf("halo %s %s %s!\n", firstName, lastName, thirdName)
-	fmt.Println("halo", firstName, lastName, thirdName + "! \n")
+	var positiveNumber uint8 = 89
+	var negativeNumber = -1243423644
 
+	fmt.Printf("bilangan positif: %d\n", positiveNumber)
+	fmt.Printf("bilangan negatif: %d\n", negativeNumber)
+
+	var decimalNumber = 2.62;
+	var decimalNumber2 float32 = 3.14;
+	var decimalNumber3 float64 = 8.999999999999999;
+	decimalNumber4 := 5.0;
+
+
+	fmt.Printf("bilangan desimal: %f\n", decimalNumber)
+	fmt.Printf("bilangan desimal: %f\n", decimalNumber2)
+	fmt.Printf("bilangan desimal: %f\n", decimalNumber3)
+	fmt.Printf("bilangan desimal: %f\n", decimalNumber4)
+	fmt.Println("bilangan desimal: ", decimalNumber);
 }
