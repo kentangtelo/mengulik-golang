610de35190772f4c8164965933a234c88ff54262
imamimaniman <namasayaiman02@gmail.com>
2024-10-31 14:39:42 +0700

    deklarasi dengan New()

--- main.go
+++ main.go
@@ -11,6 +11,29 @@ func main()  {
 
 	thirdName := "Smith";
 
+	var first, second, third string;
+	first, second, third = "John", "Doe", "Smith";
+
+	var fourth, fifth, sixth string = "John1", "Doe1", "Smith1";
+
+	seventh, eighth, ninth := "John2", "Doe2", "Smith2";
+
+	one, isFriday, twoPointTwo, say := 1, true, 2.2, "hello";
+
+	name := new(string)
+	boolEan := new(bool)
+	intEger := new(int)
+
+	fmt.Println("name string", name);
+	fmt.Println("boolEan bool", boolEan);
+	fmt.Println("intEger int", intEger);
+
+	fmt.Println(firstName, lastName, thirdName);
+	fmt.Println(first, second, third);
+	fmt.Println(fourth, fifth, sixth);
+	fmt.Println(seventh, eighth, ninth);
+	fmt.Println(one, isFriday, twoPointTwo, say);
+
 	fmt.Printf("halo %s %s %s!\n", firstName, lastName, thirdName)
 	fmt.Println("halo", firstName, lastName, thirdName + "! \n")
 
